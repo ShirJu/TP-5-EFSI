10adcaf62504a849066b84b90073561e0791cb47
MAIA RAITER <47129177@ort>
2023-06-22 11:57:01 -0300

    bien

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,44 +2,69 @@
 import './App.css';
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import Respuesta from './Respuesta.js'; 
+
 
 function App() {
 
   const [banderas, setBanderas] = useState([]);
   const [bandera, setBandera] = useState([]);
+  const [handleIniciar, setHandleInciar] = useState(false);
+  const [banderaRandom, setBanderaRandom] = useState(null);
+  const [input, setInput] = useState("");
   const respuesta="";
 
-  const ListarBanderas = () => {
+  const ListarBanderas = async() => {
     axios
       .get("https://countriesnow.space/api/v0.1/countries/flag/images")
       .then((result) => {
-        const banderas = result.data.data;
-        setBanderas(banderas);
+        setBanderas(result.data.data);
 
-        const siguienteBandera = () => {
-        var item = banderas[Math.floor(Math.random()*banderas.length)];
-        setBandera(item);
-        }
-        
-       
       })
   }
+
+  const handleChange = (e) => {
+    setInput(e.target.value)
+  }
+
+  const cargarBanderaRandom = () => {
+  
+    setHandleInciar(true);
+    var random = [Math.floor(Math.random()*banderas.length)];
+    setBanderaRandom(banderas[random]);
+    }
 
   useEffect(() => {
     ListarBanderas()
   }, []);
 
- 
+  const verificar = () => {
+    
+    if (input == banderaRandom.name){
+      setInput("");
+      cargarBanderaRandom();
+    }
+  }
   
   
 
   return (
     <>
-    <p>{bandera.name}</p>
-    <img src={bandera.flag} alt=" " />
-    <Respuesta NombreBandera={bandera.name}/>
-    <button onClick={siguienteBandera()} class="btn btn-success"> Siguiente Bandera</button>
+    {!handleIniciar && (
+      <div>
+        <button onClick={() => cargarBanderaRandom()}>Iniciar Juego</button>
+      </div>
+    )}
+    {handleIniciar && (
+      <div className='container'>
+        <p>{banderaRandom.name}</p>
+         <img src={banderaRandom.flag} alt="" />
+         <input value={input} onChange={handleChange}></input><button className='btn btn-primary' onClick={() => verificar()}>Ok</button><button className='btn btn-danger ' onClick={() => cargarBanderaRandom()}>Saltar</button>
+      </div>
+    )}
+
+    
+    
+   
     </>
   );
 }
